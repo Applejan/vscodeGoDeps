7c2296f260f166a3c0e3a7a69adeb3ac8bf54fd7
Applejam <924904427@qq.com>
2018-08-15 06:42:29 +0800

    object files

--- refresh.go
+++ refresh.go
@@ -4,20 +4,30 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 //g save the url and the target dir
-type g struct {
+type body struct {
 	url   string
 	toDir string
 }
 
-func downTOOLS() {
-	tools := g{
+func (s *body) downTOOLS() {
+	tmp := strings.Split(s.toDir, "/")
+	tmp = tmp[1:]
+	// log.Println(tmQp)
+	saveDir := os.Getenv("GOPATH") + "\\" + filepath.Join(tmp...)
+	args := []string{"pull", saveDir}
+	// log.Println(args)
+	cmd := exec.Command("git", args...)
+	cmd.Run()
+}
+func main() {
+
+	tools := body{
 		url:   "https://github.com/golang/tools",
 		toDir: "$GOPATH/src/golang.org/x/tools",
 	}
-	k := filepath.SplitList(tools.toDir)
-	saveDir := os.Getenv("GOPATH") + filepath.Join(k...)
-	cmd := exec.Command("git", "pull", tools.url, k...)
+	tools.downTOOLS()
 }
